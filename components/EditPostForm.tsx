c25a6cb6a4a657fe54749548567540d1737e0c48
KingNatan <natanaelac04@gmail.com>
2023-12-08 10:07:56 +0700

    first commit

--- components/EditPostForm.tsx
+++ components/EditPostForm.tsx
@@ -1,6 +1,6 @@
-'use client';
+'use client'
 
-import { TCategory } from "@/app/types";
+import { TCategory, TPost } from "@/app/types";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
@@ -8,7 +8,7 @@ import { CldUploadButton, CldUploadWidgetResults } from "next-cloudinary";
 import Image from "next/image";
 import toast from "react-hot-toast";
 
-export default function CreatePostForm() {
+export default function EditPostForm({ post }: { post: TPost} ) {
       const [links, setLinks] = useState<string[]>([]);
       const [linkInput, setLinkInput] = useState("");
       const [title, setTitle] = useState("");
@@ -22,27 +22,31 @@ export default function CreatePostForm() {
 
       useEffect(() => {
         const fetchAllCategories = async () => {
-            const res = await fetch("api/categories");
+            const res = await fetch("/api/categories");
             const catNames = await res.json();
             setCategories(catNames);
         };
 
         fetchAllCategories();
-      }, []);
 
-      const handleImageUpload = (result: CldUploadWidgetResults) => {
-        console.log('result', result);
-        const info = result.info as object;
+        const initValues = () => {
+            setTitle(post.title)
+            setContent(post.content);
+            setImageUrl(post.imageUrl || '');
+            setPublicId(post.publicId || '');
+            setSelectedCategory(post.catName || '');
+            setLinks(post.links || []);
+        };
 
-        if('secure_url' in info && 'public_id' in info){
-            const url = info.secure_url as string;
-            const public_id = info.public_id as string;
-            setImageUrl(url);
-            setPublicId(public_id);
-            console.log("url: ", url);
-            console.log("public_id", public_id);
-        }
-      };
+        initValues();
+      }, [
+        post.title, 
+        post.content, 
+        post.imageUrl, 
+        post.publicId, 
+        post.catName,
+        post.links,
+    ]);
 
     const addLink = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.preventDefault();
@@ -53,40 +57,52 @@ export default function CreatePostForm() {
     };
 
     const deleteLink = (index: number) => {
-        setLinks((prev) => prev.filter((_, i) => index !== index));
+        setLinks((prev) => prev.filter((_, i) => i !== index));
+    };
+
+    const handleImageUpload = (result: CldUploadWidgetResults) => {
+      const info = result.info as object;
+
+      if("secure_url" in info && "public_id" in info){
+          const url = info.secure_url as string;
+          const public_id = info.public_id as string;
+          setImageUrl(url);
+          setPublicId(public_id);
+          console.log("url: ", url);
+          console.log("public_id: " , public_id);
+      }
     };
 
     const removeImage = async (e: React.FormEvent) => {
-        e.preventDefault();
+      e.preventDefault();
 
-        try {
-            const res = await fetch("api/removeImage", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ publicId }),
-            });
-    
-            if(res.ok) {
-                setImageUrl("");
-                setPublicId("");
-            }
-        }   catch (error) {
-            console.log(error);
-        }
-    };
+      try {
+          const res = await fetch("/api/removeImage", {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({ publicId }),
+          });
+  
+          if(res.ok) {
+              setImageUrl("");
+              setPublicId("");
+          }
+      }   catch (error) {
+          console.log(error);
+      }
+  };
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
 
         if (!title || !content) {
-            const errorMessage = "Title and content are required";
-            toast.error(errorMessage);
+          toast.error("Title and content are required");
             return;
         }
 
         try {
-            const res = await fetch("api/posts/", {
-                method: "POST",
+            const res = await fetch(`/api/posts/${post.id}`, {
+                method: "PUT",
                 headers: {
                     "Content-type": "application/json"
                 },
@@ -101,13 +117,12 @@ export default function CreatePostForm() {
             });
 
             if (res.ok) {
-                router.push('dashboard');
-                router.refresh();
-                toast.success("Post created successfully");
-            } else {
-                toast.error("something went wrong");
+              toast.success("Post edited successfully");
+              router.push('/dashboard');
+              router.refresh();
             }
         }   catch (error) {
+            toast.error("Something went wrong");
             console.log(error);
         }
     };
@@ -120,52 +135,54 @@ export default function CreatePostForm() {
             onChange={e => setTitle(e.target.value)} 
             type="text" 
             placeholder="Title"
+            value={title}
             />
             <textarea 
             onChange={e => setContent(e.target.value)} 
             placeholder="Content"
+            value={content}
             ></textarea>
 
-            {links && 
-                links.map((link, i) => (
-                 <div key={i} className="flex items-center gap-4">
-                    <span>
-                        <svg 
-                        xmlns="http://www.w3.org/2000/svg" 
-                        fill="none" 
-                        viewBox="0 0 24 24" 
-                        strokeWidth={1.5} 
-                        stroke="currentColor" 
-                        className="w-6 h-6"
-                        >
-                            <path 
-                            strokeLinecap="round" 
-                            strokeLinejoin="round" 
-                            d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" 
-                            />
-                        </svg>
-                    </span>
-                     <Link className="link" href={link}>
-                        {link}
-                     </Link>
-                     <span className="cursor-pointer" onClick={() => deleteLink(i)}>
-                     <svg
-                      xmlns="http://www.w3.org/2000/svg" 
-                      fill="none" 
-                      viewBox="0 0 24 24" 
-                      strokeWidth={1.5} 
-                      stroke="currentColor" 
-                      className="w-6 h-6"
-                      >
-                        <path 
-                        strokeLinecap="round" 
-                        strokeLinejoin="round" 
-                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" 
-                        />
-                    </svg>
-                     </span>
-                 </div>
-            ))}
+        {links &&
+          links.map((link, i) => (
+            <div key={i} className="flex items-center gap-4">
+              <span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+                  />
+                </svg>
+              </span>
+              <Link className="link" href={link}>
+                {link}
+              </Link>
+              <span className="cursor-pointer" onClick={() => deleteLink(i)}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                  />
+                </svg>
+              </span>
+            </div>
+          ))}
 
             <div className="flex gap-2">
                 <input 
@@ -233,21 +250,22 @@ export default function CreatePostForm() {
             </button>
             )}
 
-            <select 
-            onChange={(e) => setSelectedCategory(e. target.value)} 
+            <select
+            onChange={e => setSelectedCategory(e.target.value)}
             className="p-3 rounded-md border appearance-none"
+            value={selectedCategory}
             >
-                <option value="">Select A Category</option>
-                {categories && 
-                    categories.map((category => (
-                    <option key={category.id} value={category.catName}>
-                        {category.catName}
-                    </option>)
+            <option value="">Select A Category</option>
+            {categories &&
+                categories.map((category) => (
+                <option key={category.id} value={category.catName}>
+                    {category.catName}
+                </option>
                 ))}
             </select>
 
             <button className="primary-btn" type="submit">
-                Create Post
+                Update Post
             </button>
         </form>
     </div>
